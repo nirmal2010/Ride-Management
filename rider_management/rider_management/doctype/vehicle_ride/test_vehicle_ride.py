9d3642ad3d4ce3184fbd546c3780102a26fe49dc
Nirmalrajaa K <nirmalrajaa@whitehatglobal.org>
2025-06-02 18:16:07 +0530

    Feat: Added new doctype to capture the Vehicle details

---
+++ rider_management/rider_management/doctype/vehicle_ride/test_vehicle_ride.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Nirmalrajaa K and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestVehicleRide(FrappeTestCase):
+	pass
